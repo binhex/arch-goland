d72434594f200b3d22222fcd7cf219a73d0da8bf
binhex <megalith01@gmail.com>
2022-11-10 09:56:04 +0000

    initial release

---
+++ README.md
@@ -0,0 +1,79 @@
+**Application**
+
+[GoLand](https://www.jetbrains.com/go/)
+
+**Description**
+
+GoLand wouldn’t be a true IDE without a rich set of tools which, in addition to core Go development, support JavaScript, TypeScript, NodeJS, SQL, Databases, Docker, Kubernetes, and Terraform. All together, these capabilities make it perfectly equipped for working on any task, be it a modern web application or DevOps tools.
+
+**Build notes**
+
+Latest stable GoLand release from Arch Linux.
+
+**Usage**
+```
+docker run -d \
+    -p 5900:5900 \
+    -p 6080:6080 \
+    --name=<container name> \
+    --security-opt seccomp=unconfined \
+    -v <path for config files>:/config \
+    -v <path for data files>:/data \
+    -v /etc/localtime:/etc/localtime:ro \
+    -e WEBPAGE_TITLE=<name shown in browser tab> \
+    -e VNC_PASSWORD=<password for web ui> \
+    -e UMASK=<umask for created files> \
+    -e PUID=<uid for user> \
+    -e PGID=<gid for user> \
+    binhex/arch-goland
+```
+
+Please replace all user variables in the above command defined by <> with the correct values.
+
+**Example**
+```
+docker run -d \
+    -p 5900:5900 \
+    -p 6080:6080 \
+    --name=goland \
+    --security-opt seccomp=unconfined \
+    -v /apps/docker/goland:/config \
+    -v /apps/docker/goland/projects:/data \
+    -v /etc/localtime:/etc/localtime:ro \
+    -e WEBPAGE_TITLE=Tower \
+    -e VNC_PASSWORD=mypassword \
+    -e UMASK=000 \
+    -e PUID=0 \
+    -e PGID=0 \
+    binhex/arch-goland
+```
+
+**Access via web interface (noVNC)**
+
+`http://<host ip>:<host port>/vnc.html?resize=remote&host=<host ip>&port=<host port>&&autoconnect=1`
+
+e.g.:-
+
+`http://192.168.1.10:6080/vnc.html?resize=remote&host=192.168.1.10&port=6080&&autoconnect=1`
+
+**Access via VNC client**
+
+`<host ip>::<host port>`
+
+e.g.:-
+
+`192.168.1.10::5900`
+
+**Notes**
+
+User ID (PUID) and Group ID (PGID) can be found by issuing the following command for the user you want to run the container as:-
+
+```
+id <username>
+```
+___
+If you appreciate my work, then please consider buying me a beer  :D
+
+[![PayPal donation](https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=MM5E27UX6AUU4)
+
+[Documentation](https://github.com/binhex/documentation) | [Support forum](https://lime-technology.com/forums/topic/62598-support-binhex-goland/)
---
+++ build/root/install.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+# exit script if return code != 0
+set -e
+
+# release tag name from build arg, stripped of build ver using string manipulation
+release_tag_name="${1//-[0-9][0-9]/}"
+
+# build scripts
+####
+
+# download build scripts from github
+curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
+
+# unzip build scripts
+unzip /tmp/scripts-master.zip -d /tmp
+
+# move shell scripts to /usr/local/bin (-n = do not overwrite
+# exsting files, required as init.sh written to via arch-int-gui)
+mv -n /tmp/scripts-master/shell/arch/docker/*.sh /usr/local/bin/
+
+# detect image arch
+####
+
+OS_ARCH=$(cat /etc/os-release | grep -P -o -m 1 "(?=^ID\=).*" | grep -P -o -m 1 "[a-z]+$")
+if [[ ! -z "${OS_ARCH}" ]]; then
+	if [[ "${OS_ARCH}" == "arch" ]]; then
+		OS_ARCH="x86-64"
+	else
+		OS_ARCH="aarch64"
+	fi
+	echo "[info] OS_ARCH defined as '${OS_ARCH}'"
+else
+	echo "[warn] Unable to identify OS_ARCH, defaulting to 'x86-64'"
+	OS_ARCH="x86-64"
+fi
+
+# pacman packages
+####
+
+# define pacman packages
+pacman_packages="git tk"
+
+# install compiled packages using pacman
+if [[ ! -z "${pacman_packages}" ]]; then
+	pacman -S --needed $pacman_packages --noconfirm
+fi
+
+# aur packages
+####
+
+# define aur packages
+aur_packages="goland goland-jre"
+
+# call aur install script (arch user repo)
+source aur.sh
+
+# config novnc
+###
+
+# overwrite novnc 16x16 icon with application specific 16x16 icon (used by bookmarks and favorites)
+cp /home/nobody/novnc-16x16.png /usr/share/webapps/novnc/app/images/icons/
+
+cat <<'EOF' > /tmp/startcmd_heredoc
+# run goland
+/usr/sbin/goland
+EOF
+
+# replace startcmd placeholder string with contents of file (here doc)
+sed -i '/# STARTCMD_PLACEHOLDER/{
+    s/# STARTCMD_PLACEHOLDER//g
+    r /tmp/startcmd_heredoc
+}' /usr/local/bin/start.sh
+rm /tmp/startcmd_heredoc
+
+# config openbox
+####
+
+cat <<'EOF' > /tmp/menu_heredoc
+    <item label="GoLand">
+    <action name="Execute">
+      <command>/usr/sbin/goland</command>
+      <startupnotify>
+        <enabled>yes</enabled>
+      </startupnotify>
+    </action>
+    </item>
+EOF
+
+# replace menu placeholder string with contents of file (here doc)
+sed -i '/<!-- APPLICATIONS_PLACEHOLDER -->/{
+    s/<!-- APPLICATIONS_PLACEHOLDER -->//g
+    r /tmp/menu_heredoc
+}' /home/nobody/.config/openbox/menu.xml
+rm /tmp/menu_heredoc
+
+# container perms
+####
+
+# define comma separated list of paths
+install_paths="/tmp,/usr/share/themes,/home/nobody,/usr/share/webapps/novnc,/usr/share/applications,/usr/share/licenses,/etc/xdg"
+
+# split comma separated string into list for install paths
+IFS=',' read -ra install_paths_list <<< "${install_paths}"
+
+# process install paths in the list
+for i in "${install_paths_list[@]}"; do
+
+	# confirm path(s) exist, if not then exit
+	if [[ ! -d "${i}" ]]; then
+		echo "[crit] Path '${i}' does not exist, exiting build process..." ; exit 1
+	fi
+
+done
+
+# convert comma separated string of install paths to space separated, required for chmod/chown processing
+install_paths=$(echo "${install_paths}" | tr ',' ' ')
+
+# set permissions for container during build - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+chmod -R 775 ${install_paths}
+
+# create file with contents of here doc, note EOF is NOT quoted to allow us to expand current variable 'install_paths'
+# we use escaping to prevent variable expansion for PUID and PGID, as we want these expanded at runtime of init.sh
+cat <<EOF > /tmp/permissions_heredoc
+
+# get previous puid/pgid (if first run then will be empty string)
+previous_puid=\$(cat "/root/puid" 2>/dev/null || true)
+previous_pgid=\$(cat "/root/pgid" 2>/dev/null || true)
+
+# if first run (no puid or pgid files in /tmp) or the PUID or PGID env vars are different
+# from the previous run then re-apply chown with current PUID and PGID values.
+if [[ ! -f "/root/puid" || ! -f "/root/pgid" || "\${previous_puid}" != "\${PUID}" || "\${previous_pgid}" != "\${PGID}" ]]; then
+
+	# set permissions inside container - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+	chown -R "\${PUID}":"\${PGID}" ${install_paths}
+
+fi
+
+# write out current PUID and PGID to files in /root (used to compare on next run)
+echo "\${PUID}" > /root/puid
+echo "\${PGID}" > /root/pgid
+
+EOF
+
+# replace permissions placeholder string with contents of file (here doc)
+sed -i '/# PERMISSIONS_PLACEHOLDER/{
+    s/# PERMISSIONS_PLACEHOLDER//g
+    r /tmp/permissions_heredoc
+}' /usr/local/bin/init.sh
+rm /tmp/permissions_heredoc
+
+# env vars
+####
+
+# cleanup
+cleanup.sh
