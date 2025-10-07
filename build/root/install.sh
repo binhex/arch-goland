bf5bbbfb02eb44450b03acf5a90338170d2a61bf
binhex <megalith01@gmail.com>
2025-10-07 10:04:53 +0100

    switch to new aur.sh

--- build/root/install.sh
+++ build/root/install.sh
@@ -55,10 +55,10 @@ fi
 ####
 
 # define aur packages
-aur_packages="goland goland-jre"
+aur_packages="goland,goland-jre"
 
 # call aur install script (arch user repo)
-source aur.sh
+aur.sh --aur-package "${aur_packages}"
 
 # config novnc
 ###
